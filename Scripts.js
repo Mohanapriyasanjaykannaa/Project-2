d30b96a349e9f95badf84b0b1c4f48818cfe2837
Mohana priya <mohanapriya.t.y@gmail.com>
2025-01-01 19:17:47 +0530

    Create Scripts.js
    Javascript of index file

---
+++ Scripts.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+      const menuBar = document.getElementById('menuBar');
+      if (menuBar.style.display === 'block') {
+        menuBar.style.display = 'none';
+      } else {
+        menuBar.style.display = 'block';
+      }
+    }
+    
